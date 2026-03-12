b1821bdddb4ebfaf14fdcc31caccbae643925417
khaled6013 <kmd601384@gmail.com>
2026-03-12 10:18:44 +0600

    comment update

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,15 @@
 function sendGuess() {
-
-    // input থেকে number নেওয়া
     let guess = document.getElementById("guess").value;
-
-    // যদি user কিছু না লিখে
     if (guess === "") {
         document.getElementById("result").innerText = "⚠️ Please enter a number!";
         return;
     }
-
-    // backend এ request পাঠানো
     fetch("http://localhost:8000/guess?number=" + guess)
 
     .then(response => response.text())
 
     .then(data => {
-
-        // result show করা
         document.getElementById("result").innerText = data;
-
-        // bonus UI color change
         if (data.includes("High")) {
             document.getElementById("result").style.color = "red";
         }
